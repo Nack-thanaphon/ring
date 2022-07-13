a2011bea0bcbc6b590f7bd0c6d3b9aa60ebf4015
Nack Thanaphon <78144742+nackdev2535@users.noreply.github.com>
2022-07-14 02:50:26 +0700

    v2

--- main.js
+++ main.js
@@ -3,10 +3,10 @@ var stickyElem = new Foundation.Sticky($(".top-bar"), {
     marginTop: 0
 });
 
-$(".top-bar").on("sticky.zf.stuckto:top", function() {
+$(".top-bar").on("sticky.zf.stuckto:top", function () {
     $(".sticky-container").fadeIn();
 });
-$(".top-bar").on("sticky.zf.unstuckfrom:top", function() {
+$(".top-bar").on("sticky.zf.unstuckfrom:top", function () {
     $(".sticky-container").fadeOut();
 });
 
@@ -21,7 +21,7 @@ $('.fade').slick({
 });
 
 
-$(document).ready(function() {
+$(document).ready(function () {
 
     $('.carousel').slick({
         dots: true,
@@ -32,3 +32,33 @@ $(document).ready(function() {
     });
 
 });
+
+$(document).ready(function () {
+    $(".carousel").slick({
+        dots: true,
+        infinite: true,
+        speed: 500,
+        fade: true,
+        cssEase: "linear",
+    });
+});
+
+$(document).ready(function () {
+    $("#product_all").slick({
+        infinite: true,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+    });
+});
+$(document).ready(function () {
+    $("#product_all2").slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 2,
+    });
+});
+
+window.onload = function () {
+    slideOne();
+    slideTwo();
+};
